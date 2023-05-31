b07ebfada67fe4deee8db754b3e570636e1c2c05
Shivam Sharma <shivsharma7821@gmail.com>
2023-05-31 18:41:35 +0530

    🎨 style(current-weather.css): increase font size of parameter-label to improve readability 🚀 perf(hourly.js): optimize hourly component by using context and mapping through hourly data to display forecast data

--- src/components/hourly/hourly.js
+++ src/components/hourly/hourly.js
@@ -1,54 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./hourly.css";
+import weatherContext from "../../context/weather/weatherContext";
 
 const Hourly = () => {
-  
+  const hourly = useContext(weatherContext);
+  const { forecast } = hourly;
+  const { hour, date } = forecast.forecastday[0];
+  // const { time, temp_c, temp_f, condition } = hour[0];
+  // const Time = time.split(`${date}`)[1];
+  // const { text, icon } = condition;
+  // const iconPath = icon.split("//cdn.weatherapi.com/weather/64x64/")[1];
 
   return (
-    // <div>
-    //   <h1 className="Hourly__title">{[date[0], date[1], date[2]]}</h1>
-    //   <img
-    //     className="Hourly__weather-icon"
-    //     src={
-    //       "https://openweathermap.org/img/wn/" +
-    //       weatherInfo.weather[0].icon +
-    //       ".png"
-    //     }
-    //     alt={weatherInfo.weather[0].main}
-    //   />
-    //   <div className="Hourly__temperature">
-    //     <span className="temperature__max">
-    //       {Math.round(weatherInfo.temp.max)}
-    //       <sup className="temperature__symbol">°</sup>
-    //     </span>
-    //     <span className="temperature__min">
-    //       {Math.round(weatherInfo.temp.min)}
-    //       <sup className="temperature__symbol">°</sup>
-    //     </span>
-    //   </div>
-    // </div>
     <div className="hourly">
-      {
-        <div className="hourly-forcast">
-          <h1 className="Hourly__title">16:00</h1>
-          <img
-            className="Hourly__weather-icon"
-            src={`icons/10d.png`}
-            alt="rainny"
-          />
-          <div className="Hourly__temperature">
-            <span className="temperature__max">
-              28
-              <sup className="temperature__symbol">°</sup>
-            </span>
-            <span className="temperature__min">
-              25
-              <sup className="temperature__symbol">°</sup>
-            </span>
+      {hour.map((data) => {
+        const { time, temp_c, temp_f, condition } = data;
+        const Time = time.split(`${date}`)[1];
+        const { text, icon } = condition;
+        const iconPath = icon.split("//cdn.weatherapi.com/weather/64x64/")[1];
+        return (
+          <div className="hourly-forcast" key={time}>
+            <h1 className="Hourly__title">{Time}</h1>
+            <img
+              className="Hourly__weather-icon"
+              src={`icons/${iconPath}`}
+              alt={text}
+            />
+            <div className="Hourly__temperature">
+              <span className="temperature__max">
+                {Math.round(temp_c)}
+                <sup className="temperature__symbol">°</sup>
+              </span>
+              <span className="temperature__min">
+                {Math.round(temp_f)}
+                <sup className="temperature__symbol">°</sup>
+              </span>
+            </div>
           </div>
-        </div>
-      }
-      <div className="hourly-forcast">
+        );
+      })}
+      {/* <div className="hourly-forcast">
         <h1 className="Hourly__title">16:00</h1>
         <img
           className="Hourly__weather-icon"
@@ -317,7 +308,7 @@ const Hourly = () => {
             <sup className="temperature__symbol">°</sup>
           </span>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
