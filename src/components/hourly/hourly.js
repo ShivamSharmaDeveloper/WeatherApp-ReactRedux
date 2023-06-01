e3b7f132cb039018cd17e0bc7c203235826c2c03
Shivam Sharma <shivsharma7821@gmail.com>
2023-06-01 12:14:29 +0530

    🎨 style(index.html): add font-awesome 6.4.0 and swiper-bundle.min.css to improve UI 🎨 style(hourly.css): improve hourly component UI by adding styles for arrows and container scrolling
    🔥 refactor(hourly.js): remove 270 lines of unused code to improve codebase maintainability and readability
    
    ♻️ refactor(hourly.js): remove commented code and add left and right arrows to navigate hourly forecast

--- src/components/hourly/hourly.js
+++ src/components/hourly/hourly.js
@@ -6,309 +6,39 @@ const Hourly = () => {
   const hourly = useContext(weatherContext);
   const { forecast } = hourly;
   const { hour, date } = forecast.forecastday[0];
-  // const { time, temp_c, temp_f, condition } = hour[0];
-  // const Time = time.split(`${date}`)[1];
-  // const { text, icon } = condition;
-  // const iconPath = icon.split("//cdn.weatherapi.com/weather/64x64/")[1];
 
   return (
     <div className="hourly">
-      {hour.map((data) => {
-        const { time, temp_c, temp_f, condition } = data;
-        const Time = time.split(`${date}`)[1];
-        const { text, icon } = condition;
-        const iconPath = icon.split("//cdn.weatherapi.com/weather/64x64/")[1];
-        return (
-          <div className="hourly-forcast" key={time}>
-            <h1 className="Hourly__title">{Time}</h1>
-            <img
-              className="Hourly__weather-icon"
-              src={`icons/${iconPath}`}
-              alt={text}
-            />
-            <div className="Hourly__temperature">
-              <span className="temperature__max">
-                {Math.round(temp_c)}
-                <sup className="temperature__symbol">°</sup>
-              </span>
-              <span className="temperature__min">
-                {Math.round(temp_f)}
-                <sup className="temperature__symbol">°</sup>
-              </span>
+      <div className="hourly-container">
+        <i id="left" className="fa-solid fa-angle-left"></i>
+        {hour.map((data) => {
+          const { time, temp_c, temp_f, condition } = data;
+          const Time = time.split(`${date}`)[1];
+          const { text, icon } = condition;
+          const iconPath = icon.split("//cdn.weatherapi.com/weather/64x64/")[1];
+          return (
+            <div className="hourly-forcast" key={time}>
+              <h1 className="Hourly__title">{Time}</h1>
+              <img
+                className="Hourly__weather-icon"
+                src={`icons/${iconPath}`}
+                alt={text}
+              />
+              <div className="Hourly__temperature">
+                <span className="temperature__max">
+                  {Math.round(temp_c)}
+                  <sup className="temperature__symbol">°</sup>
+                </span>
+                <span className="temperature__min">
+                  {Math.round(temp_f)}
+                  <sup className="temperature__symbol">°</sup>
+                </span>
+              </div>
             </div>
-          </div>
-        );
-      })}
-      {/* <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
+          );
+        })}
+        <i id="right" className="fa-solid fa-angle-right"></i>
       </div>
-      <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
-      </div>
-      <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
-      </div>
-      <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
-      </div>
-      <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
-      </div>
-      <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
-      </div>
-      <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
-      </div>
-      <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
-      </div>
-      <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
-      </div>
-      <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
-      </div>
-      <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
-      </div>
-      <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
-      </div>
-      <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
-      </div>
-      <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
-      </div>
-      <div className="hourly-forcast">
-        <h1 className="Hourly__title">16:00</h1>
-        <img
-          className="Hourly__weather-icon"
-          src={`icons/10d.png`}
-          alt="rainny"
-        />
-        <div className="Hourly__temperature">
-          <span className="temperature__max">
-            28
-            <sup className="temperature__symbol">°</sup>
-          </span>
-          <span className="temperature__min">
-            25
-            <sup className="temperature__symbol">°</sup>
-          </span>
-        </div>
-      </div> */}
     </div>
   );
 };
