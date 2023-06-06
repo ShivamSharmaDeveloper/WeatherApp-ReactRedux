ce158b08ae2d5137105a05074fb329eef9ce84d9
Shivam Sharma <shivsharma7821@gmail.com>
2023-06-06 16:43:57 +0530

    :recycle: Refactor WeatherState component - Updated fetchData function to extract and structure weather data - Updated setLocation function to use fetchData with the provided location - Updated useEffect to call fetchData with default location

--- src/context/weather/WeatherState.js
+++ src/context/weather/WeatherState.js
@@ -7,19 +7,53 @@ const WeatherState = ({ children }) => {
   // const [location, setLocation] = useState("Gurgaon");
   const API_KEY = process.env.REACT_APP_API_KEY;
   // console.log(location);
-  const setLocation = (location) => {
-    fetchData(location);
-  };
 
-  const fetchData = async (location) => {
+  const fetchData = async (actuallocation) => {
     try {
       const response = await axios.get(
-        `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=10&aqi=no&alerts=no`
+        `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${actuallocation}&days=10&aqi=no&alerts=no`
       );
-      setWeatherData(response.data);
+      const { location, current, forecast } = response.data;
+      const currentLocation = {
+        name: location.name,
+        region: location.region,
+      };
+
+      const currentWeather = {
+        temp_c: current.temp_c,
+        feelslike_c: current.feelslike_c,
+        humidity: current.humidity,
+        text: current.condition.text,
+        icon: current.condition.icon,
+        date: forecast.forecastday[0].date,
+        hour: forecast.forecastday[0].hour,
+      };
+
+      const forecastWeather = forecast.forecastday.map((day) => ({
+        date: day.date,
+        temp_c: day.day.avgtemp_c,
+        maxtemp_c: day.day.maxtemp_c,
+        mintemp_c: day.day.mintemp_c,
+        humidity: day.day.avghumidity,
+        wind: day.day.maxwind_kph,
+        text: day.day.condition.text,
+        icon: day.day.condition.icon,
+      }));
+
+      setWeatherData({
+        location: currentLocation,
+        current: currentWeather,
+        forecast: forecastWeather,
+      });
+
+      // setWeatherData(response.data);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const setLocation = (defLocation) => {
+    fetchData(defLocation);
   };
 
   useEffect(() => {
